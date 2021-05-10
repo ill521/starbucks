147c6ff09d36aa92ef92d9916158d4229d15fcde
ill521 <ill5213215@gmail.com>
2021-05-10 13:48:21 +0900

    Start project

---
+++ js/youtube.js
@@ -0,0 +1,26 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+
+function onYouTubeIframeAPIReady() {
+  new YT.Player('player', {
+    videoId: 'An6LvWQuj_8', //최초 재생할 유튜브 영상 ID
+    playerVars: {
+      autoplay: true,
+      loop: true,
+      playlist: 'An6LvWQuj_8'
+    },
+    events: {
+      onReady:function (event) {
+        event.target.mute() //음소거
+      }
+    }
+    
+  });
+}
